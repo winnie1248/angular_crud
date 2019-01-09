e20ba8ef442b0f388223cca1c70b1022aa599cb5
winnie1248 <serenade0936@gmail.com>
2019-01-09 21:36:19 +0800

    add search user feature

--- src/app/users/users-query/users-query.component.ts
+++ src/app/users/users-query/users-query.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Users } from '../../shared/model';
+import { UsersService } from '../../shared/service/users.service';
+
 @Component({
   selector: 'app-users-query',
   templateUrl: './users-query.component.html',
   styleUrls: ['./users-query.component.scss']
 })
 export class UsersQueryComponent implements OnInit {
+  users: Users;
 
-  constructor() { }
+  constructor(
+    private usersService: UsersService
+  ) { }
 
   ngOnInit() {
+    this.usersService.query().subscribe((users: Users) => {
+      this.users = users;
+    });
   }
 
 }
