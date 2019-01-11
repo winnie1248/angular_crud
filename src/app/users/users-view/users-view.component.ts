dd173f26201ae0ef45d8783b79099516454de50b
winnie1248 <serenade0936@gmail.com>
2019-01-12 01:15:05 +0800

    add view user info feature

--- src/app/users/users-view/users-view.component.ts
+++ src/app/users/users-view/users-view.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+import { User, DefaultUser } from '../../shared/model';
+import { UsersService } from '../../shared/service/users.service';
 
 @Component({
   selector: 'app-users-view',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-view.component.scss']
 })
 export class UsersViewComponent implements OnInit {
+  user: User = new DefaultUser();
 
-  constructor() { }
+  constructor(
+    private _router: Router,
+    private _activatedRoute: ActivatedRoute,
+    private usersService: UsersService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this._activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      const userId = paramMap.get('userId');
+      this.usersService.get(userId).subscribe((user: User) => {
+        this.user = user;
+      }, (error: Error) => {
+        alert(error);
+      });
+    });
   }
+
+  // private redirect(userId: string): void {
+  //   const path = this.viewType === 'edit' ? '../../view/' + userId : '../view/' + userId;
+  //   this._router.navigate([path], {
+  //     relativeTo: this._activatedRoute,
+  //     queryParamsHandling: 'preserve'
+  //   });
+  // }
 
 }
