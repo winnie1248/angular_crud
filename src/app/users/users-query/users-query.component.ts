1d05d47444ac550d6e847c0edf277e243d0decc9
winnie1248 <serenade0936@gmail.com>
2019-01-12 01:15:37 +0800

    add delete user feature

--- src/app/users/users-query/users-query.component.ts
+++ src/app/users/users-query/users-query.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { tap, flatMap } from 'rxjs/operators';
 
 import { Users } from '../../shared/model';
 import { UsersService } from '../../shared/service/users.service';
@@ -19,6 +20,14 @@ export class UsersQueryComponent implements OnInit {
     this.usersService.query().subscribe((users: Users) => {
       this.users = users;
     });
+  }
+
+  deleteUser(userId: string) {
+    this.usersService.delete([userId]).pipe(
+      flatMap(() => this.usersService.query().pipe(
+        tap((users: Users) => this.users = users)
+      ))
+    ).subscribe();
   }
 
 }
